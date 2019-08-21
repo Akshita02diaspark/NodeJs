099423d2d8e8f5be93cfdcacdf89113445b495b3
Akshita02diaspark <51989499+Akshita02diaspark@users.noreply.github.com>
2019-08-21 12:19:29 +0530

    Add files via upload

---
+++ random_quotes/javascript.js
@@ -0,0 +1,26 @@
+const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+
+function getQuote() {
+  fetch(endpoint)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayQuote(data.message);
+    })
+    .catch(function () {
+      console.log("An error occurred");
+    });
+}
+
+function displayQuote(quote) {
+  const quoteText = document.querySelector('.quote-text');
+  quoteText.textContent = quote;
+
+  alert(quote);
+}
+
+const newQuoteButton = document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+getQuote();
